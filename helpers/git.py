06d5ce92bac8650aca90826b060ceb2adcbdb1c4
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2014-04-15 12:03:29 +0100

    git helpers: added exception catching for yaml parsting

--- helpers/git.py
+++ helpers/git.py
@@ -31,55 +31,58 @@ def copy_commits_to_db(r, repository, lastsha=None):
             # process only yaml files
             # ignore symlinks
             if blob.name.endswith("yaml") and blob.mode == blob.file_mode:
-                tc = yaml.load(blob.data_stream.read())
-                test_id = tc['metadata']['name']
-                test_name = tc['metadata']['name'] + "@" + blob.path.rstrip(blob.name).rstrip("/")
-                test_file_name = blob.path
-                test_description = tc['metadata']['description']
-                test_defaults = {
-                    'repository': repository,
-                    'test_file_name': test_file_name,
-                    'description': test_description,
-                }
-                newtest, created = TestDefinition.objects.get_or_create(
-                    name=test_name, 
-                    test_id=test_id, 
-                    defaults=test_defaults)
-                tc_maintainer_list = extract_metadata(tc['metadata'], 'maintainer', Maintainer, 'email')
-                test_defaults.update({'maintainer': tc_maintainer_list})
-    
-                tc_os_list = extract_metadata(tc['metadata'], 'os', OS)
-                test_defaults.update({'os': tc_os_list})
-    
-                tc_scope_list = extract_metadata(tc['metadata'], 'scope', Scope)
-                test_defaults.update({'scope': tc_scope_list})
-    
-                tc_device_list = extract_metadata(tc['metadata'], 'devices', Device)
-                test_defaults.update({'device': tc_device_list})
-    
-                if not created:
-                    for attr, value in test_defaults.iteritems():
-                        attribute = getattr(newtest, attr)
-                        if isinstance(newtest._meta.get_field_by_name(attr)[0],
-                                      ManyToManyField):
-                            if set([x.pk for x in attribute.all()]) != set(value):
-                                log.debug("{0} old: {1}, new: {2}".format(
-                                    attr, [x.pk for x in attribute.all()], value))
-                                setattr(newtest, attr, value)
-                        else:
-                            if attribute != value:
-                                log.debug("{0} old: {1}, new: {2}".format(
-                                    attr, getattr(newtest, attr), value))
-                                setattr(newtest, attr, value)
-                else:
-                    newtest.maintainer = tc_maintainer_list
-                    newtest.os = tc_os_list
-                    newtest.scope = tc_scope_list
-                    newtest.device = tc_device_list
-                newtest.save()
-                revision, created = TestDefinitionRevision.objects.get_or_create(revision=commit.hexsha)
-                if newtest not in revision.test_definition.all():
-                    revision.test_definition.add(newtest)
+                try:
+                    tc = yaml.load(blob.data_stream.read())
+                    test_id = tc['metadata']['name']
+                    test_name = tc['metadata']['name'] + "@" + blob.path.rstrip(blob.name).rstrip("/")
+                    test_file_name = blob.path
+                    test_description = tc['metadata']['description']
+                    test_defaults = {
+                        'repository': repository,
+                        'test_file_name': test_file_name,
+                        'description': test_description,
+                    }
+                    newtest, created = TestDefinition.objects.get_or_create(
+                        name=test_name, 
+                        test_id=test_id, 
+                        defaults=test_defaults)
+                    tc_maintainer_list = extract_metadata(tc['metadata'], 'maintainer', Maintainer, 'email')
+                    test_defaults.update({'maintainer': tc_maintainer_list})
+        
+                    tc_os_list = extract_metadata(tc['metadata'], 'os', OS)
+                    test_defaults.update({'os': tc_os_list})
+        
+                    tc_scope_list = extract_metadata(tc['metadata'], 'scope', Scope)
+                    test_defaults.update({'scope': tc_scope_list})
+        
+                    tc_device_list = extract_metadata(tc['metadata'], 'devices', Device)
+                    test_defaults.update({'device': tc_device_list})
+        
+                    if not created:
+                        for attr, value in test_defaults.iteritems():
+                            attribute = getattr(newtest, attr)
+                            if isinstance(newtest._meta.get_field_by_name(attr)[0],
+                                          ManyToManyField):
+                                if set([x.pk for x in attribute.all()]) != set(value):
+                                    log.debug("{0} old: {1}, new: {2}".format(
+                                        attr, [x.pk for x in attribute.all()], value))
+                                    setattr(newtest, attr, value)
+                            else:
+                                if attribute != value:
+                                    log.debug("{0} old: {1}, new: {2}".format(
+                                        attr, getattr(newtest, attr), value))
+                                    setattr(newtest, attr, value)
+                    else:
+                        newtest.maintainer = tc_maintainer_list
+                        newtest.os = tc_os_list
+                        newtest.scope = tc_scope_list
+                        newtest.device = tc_device_list
+                    newtest.save()
+                    revision, created = TestDefinitionRevision.objects.get_or_create(revision=commit.hexsha)
+                    if newtest not in revision.test_definition.all():
+                        revision.test_definition.add(newtest)
+                except:
+                    log.error("problem with {0} and {1}".format(commit.hexsha, blob.name))
         log.info("updated head revision to: {0}".format(commit.hexsha))
         repository.head_revision = commit.hexsha
         repository.save()
