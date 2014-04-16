5e514f3454845f5703c307dc459f687e0262e6eb
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2014-04-16 12:40:01 +0100

    testrunner: non-matrix builds are also supported now

--- testrunner/management/commands/fetch_matrix_builds.py
+++ testrunner/management/commands/fetch_matrix_builds.py
@@ -18,12 +18,15 @@ log = logging.getLogger('testrunner')
 class Command(BaseCommand):
     def handle(self, *args, **options):
         log.setLevel(logging.DEBUG)
-        #jenkins_device_regexp = re.compile(ur'%s\s\xc2\xbb\s(?P<device_name>\w+)' % (project), re.UNICODE)
         for job in JenkinsJob.objects.all():
             log.debug("processing {0}".format(job.name))
             jenkins = Jenkins(job.service.url)
             jenkins_job = jenkins[job.name]
             last_db_build = job.builds.filter(is_umbrella=True).order_by('-number')
+            if not last_db_build:
+                # make sure there are not builds if this is not a matrix type job
+                # if there are no builds at all, the result is the same
+                last_db_build = job.builds.filter(is_umbrella=False).order_by('-number')
             last_jenkins_build = jenkins_job.get_last_buildnumber()
             if last_db_build: 
                 if last_db_build[0].number < last_jenkins_build:
