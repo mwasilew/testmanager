b451eb5aa74203db87baad7c0b4dccb3b1ca977f
xando <sebastian.pawlus@gmail.com>
2014-06-29 10:16:29 +0200

    imports fix

--- testmanager/testrunner/urls.py
+++ testmanager/testrunner/urls.py
@@ -17,16 +17,24 @@
 # along with Testmanager.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf.urls import patterns, url, include
-from testmanager.testrunner.views import *
+from testmanager.testrunner import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', index),
-    url(r'^jenkins_job/(?P<job_name>[0-9a-zA-Z_\-]+)$', jenkins_job_view, name='jenkins_job_view'),
-    #url(r'^jenkins_job/build/(?P<build_name>[a-zA-Z_\-]+)', jenkins_build_list, name='jenkins_build_list'),
-    url(r'^jenkins_job/(?P<job_name>[0-9a-zA-Z_\-]+)/build/(?P<build_number>\d+)$', jenkins_build_view, name='jenkins_build_view'),
-    url(r'^jenkins_job/(?P<job_name>[0-9a-zA-Z_\-]+)/build/(?P<build_number>\d+)/(?P<lava_job_number>\d+)$', lava_job_view, name='lava_job_view'),
+    url(r'^$', views.index),
+
+    url(r'^jenkins_job/(?P<job_name>[0-9a-zA-Z_\-]+)$',
+        views.jenkins_job_view, name='jenkins_job_view'),
+
+    # url(r'^jenkins_job/build/(?P<build_name>[a-zA-Z_\-]+)',
+    #     jenkins_build_list, name='jenkins_build_list'),
+
+    url(r'^jenkins_job/(?P<job_name>[0-9a-zA-Z_\-]+)/build/(?P<build_number>\d+)$',
+        views.jenkins_build_view, name='jenkins_build_view'),
+
+    url(r'^jenkins_job/(?P<job_name>[0-9a-zA-Z_\-]+)/build/(?P<build_number>\d+)/(?P<lava_job_number>\d+)$',
+        views.lava_job_view, name='lava_job_view'),
 
     # compare test results
-    url(r'^compare_results/$', compare_results, name='compare_results'),
+    url(r'^compare_results/$', views.compare_results, name='compare_results'),
 )
