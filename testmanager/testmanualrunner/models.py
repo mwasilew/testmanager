2759b7eb1fd4e5ec43ca8f7ac5d28e36f9652063
xando <sebastian.pawlus@gmail.com>
2014-07-03 11:36:56 +0200

    missing fields

--- testmanager/testmanualrunner/models.py
+++ testmanager/testmanualrunner/models.py
@@ -5,12 +5,20 @@ class TestRun(models.Model):
     test_plan = models.ForeignKey('testplanner.TestPlan')
     build = models.ForeignKey('testrunner.JenkinsBuild')
 
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class TestRunResult(models.Model):
     test_run = models.ForeignKey('TestRun')
     test_definition = models.ForeignKey('testplanner.TestDefinition')
 
-    created_at = models.DateField(auto_now_add=True)
+    status = models.ForeignKey('TestStatus')
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class TestStatus(models.Model):
+    name = models.CharField(max_length=36)
+    color = models.CharField(max_length=36)
