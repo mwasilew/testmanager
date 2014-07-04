d5e22f26a3f617f08746f2173a89337f80fee569
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2014-07-04 11:28:34 +0100

    fixes in the data digging scripts
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- helpers/jenkins_lava.py
+++ helpers/jenkins_lava.py
@@ -20,8 +20,11 @@ import re
 import ast
 import json
 import logging
+import requests
 import traceback
-from urllib2 import URLError
+import xmlrpclib
+from requests.exceptions import ConnectionError
+#from urllib2 import URLError
 from datetime import datetime
 from django.conf import settings
 from testmanager.testplanner.models import (
@@ -40,70 +43,117 @@ from testmanager.testrunner.models import (
     LavaJobTestResult,
     LavaJobTestResultUnit
 )
-#ToDo - get rid of lava_tool. Rewrite XML-RPC using token stored in settings
-from lava_tool.authtoken import (
-    AuthenticatingServerProxy,
-    KeyringAuthBackend
-)
 
 RUNNING = "RUNNING"
 ERROR = "ERROR"
 log = logging.getLogger('testrunner')
 
+
+class LavaServerException(Exception):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return repr(self.name)
+
+
+class LavaServer(object):
+
+    def __init__(self, lava_url, username, token):
+        self.lava_url = lava_url
+        self.username = username
+        self.token = token
+ 
+    def call_xmlrpc(self, method_name, *method_params):
+        payload = xmlrpclib.dumps((method_params), method_name)
+        
+        response = requests.request('POST', self.lava_url,
+                                    data = payload,
+                                    headers = {'Content-Type': 'application/xml'},
+                                    auth = (self.username, self.token),
+                                    timeout = 100,
+                                    stream = False)
+        
+        if response.status_code == 200:
+            result = xmlrpclib.loads(response.content)[0][0]
+            return result
+        else:
+            raise LavaServerException(response.status_code)
+
+
 def fetch_jenkins_builds(jenkins_db_job, jenkins_job, jenkins_build):
-    lava_server = AuthenticatingServerProxy(
-        settings.LAVA_SERVER_URL,
-        verbose=False,
-        auth_backend=KeyringAuthBackend())
+    lava_server = LavaServer(
+         settings.LAVA_SERVER_URL, 
+         settings.LAVA_SERVER_USERNAME, 
+         settings.LAVA_SERVER_TOKEN)
 
     lava_job_regexp = re.compile(settings.LAVA_JOB_ID_REGEXP)
-    build = jenkins_job.get_build(jenkins_build)
+    build = None
+    try:
+        build = jenkins_job.get_build(jenkins_build)
+    except ConnectionError:
+        logger.error("ConnectionError when fetchin Jenkins build: {0}".format(jenkins_build))
+        return 
     # create umbrella build in DB
     db_build = create_jenkins_build(build, jenkins_db_job)
     log.debug("master build: {0} {1}".format(db_build.number, db_build.name))
     is_matrix = False
     log.debug("fetching matrix builds")
-    for run in build.get_matrix_runs():
-        is_matrix = True
-        db_run = create_jenkins_build(run, jenkins_db_job, False, db_build)
-        if 'description' in run._data and run._data['description']:
-            log.debug("Jenkins build description: {0}".format(run._data['description']))
-            for r in lava_job_regexp.finditer(run._data['description']):
-                log.debug('LAVA job ID: {0}'.format(r.group('lava_job_id')))
-                get_lava_job_details(r.group('lava_job_id'), db_run, lava_server)
-        else:
-            log.debug("no description for build")
-            for r in lava_job_regexp.finditer(run.get_console()):
-                log.debug('LAVA job ID: {0}'.format(r.group('lava_job_id')))
-                get_lava_job_details(r.group('lava_job_id'), db_run, lava_server)
-    if not is_matrix:
-        db_run = create_jenkins_build(build, jenkins_db_job, False, db_build)
-        if 'description' in build._data and build._data['description']:
-            log.debug("Jenkins build description: {0}".format(build._data['description']))
-            for r in lava_job_regexp.finditer(build._data['description']):
-                #print 'LAVA job ID:', r.group('lava_job_id')
-                get_lava_job_details(r.group('lava_job_id'), db_run, lava_server)
-
+    try:
+        for run in build.get_matrix_runs():
+            is_matrix = True
+            # mitigate Jenkins API bug. Sometimes API contains old builds
+            if db_build.number == run.get_number():
+                db_run = create_jenkins_build(run, jenkins_db_job, False, db_build)
+                if db_run:
+                    if 'description' in run._data and run._data['description']:
+                        log.debug("Jenkins build description: {0}".format(run._data['description']))
+                        for r in lava_job_regexp.finditer(run._data['description']):
+                            log.debug('LAVA job ID: {0}'.format(r.group('lava_job_id')))
+                            get_lava_job_details(r.group('lava_job_id'), db_run, lava_server)
+                    else:
+                        log.debug("no description for build")
+                        for r in lava_job_regexp.finditer(run.get_console()):
+                            log.debug('LAVA job ID: {0}'.format(r.group('lava_job_id')))
+                            get_lava_job_details(r.group('lava_job_id'), db_run, lava_server)
+            else:
+                log.warning(
+                    "Matrix build run {0} comes from other master build".format(
+                        run.get_number()))
+        if not is_matrix:
+            db_run = create_jenkins_build(build, jenkins_db_job, False, db_build)
+            if db_run:
+                if 'description' in build._data and build._data['description']:
+                    log.debug("Jenkins build description: {0}".format(build._data['description']))
+                    for r in lava_job_regexp.finditer(build._data['description']):
+                        get_lava_job_details(r.group('lava_job_id'), db_run, lava_server)
+    except ConnectionError:
+        log.error("ConnectionError occured. Probably timeout")
+        log.error(traceback.print_exc())
+        build_status, created = JenkinsBuildStatus.objects.get_or_create(name = ERROR)
+        db_build.status = build_status
+        db_build.save()
 
 
 def get_lava_job_details(job_id, jenkins_build, lava_server=None):
     if lava_server is None:
-        lava_server = AuthenticatingServerProxy(
-            settings.LAVA_SERVER_URL,
-            verbose=False,
-            auth_backend=KeyringAuthBackend())
-
+        lava_server = LavaServer(
+             settings.LAVA_SERVER_URL, 
+             settings.LAVA_SERVER_USERNAME, 
+             settings.LAVA_SERVER_TOKEN)
+       
     try:
-        job_details = lava_server.scheduler.job_details(job_id)
+        job_details = lava_server.call_xmlrpc("scheduler.job_details", job_id)
         #pprint(job_details)
         db_device = None
         if 'requested_device_type_id' in job_details and job_details['requested_device_type_id']:
-            db_device, created = Device.objects.get_or_create(name = job_details['requested_device_type_id'])
+            db_device, created = Device.objects.get_or_create(
+                name = job_details['requested_device_type_id'])
         else:
             # try to decode the device type from the device name
             # it is assumed that device ID ends with 2 digit number added to device type
             db_device, created = Device.objects.get_or_create(name = job_details['actual_device_id'][0:-2])
-        job_status = lava_server.scheduler.job_status(job_id)
+        job_status = lava_server.call_xmlrpc("scheduler.job_status", job_id)
         #print 'LAVA job status:', job_status['job_status']
         lava_db_job_status, created = LavaJobStatus.objects.get_or_create(name = job_status['job_status'])
         lava_job_defaults = {
@@ -159,7 +209,7 @@ def get_lava_job_details(job_id, jenkins_build, lava_server=None):
 
         if 'bundle_sha1' in job_status and job_status['bundle_sha1']:
             bundle_hash = job_status['bundle_sha1']
-            bundle = json.loads(lava_server.dashboard.get(bundle_hash)['content'])
+            bundle = json.loads(lava_server.call_xmlrpc("dashboard.get", bundle_hash)['content'])
             log.debug("Test runs executed: {0}".format(len(bundle['test_runs'])))
             for run in iter(bundle['test_runs']):
                 if run['test_id'] != 'lava':
@@ -179,6 +229,7 @@ def get_lava_job_details(job_id, jenkins_build, lava_server=None):
                         # check if there is a corresponding version in db
                         test_definition_revision_list = result_test_definition.testdefinitionrevision_set.filter(revision = run['testdef_metadata']['version'])
                         if test_definition_revision_list:
+                            log.debug("assigning test definition revision with sha1: {0}".format(test_definition_revision_list[0]))
                             lava_db_result.test_revision = test_definition_revision_list[0]
 
                     lava_db_result.save()
@@ -196,7 +247,7 @@ def get_lava_job_details(job_id, jenkins_build, lava_server=None):
                             lava_db_test_result.value = test_result['measurement']
                             lava_db_test_result.unit = db_unit
                         lava_db_test_result.save()
-    except URLError:
+    except ConnectionError:
         log.error("URLError occured. Probably timeout")
         log.error(traceback.print_exc())
         build_status, created = JenkinsBuildStatus.objects.get_or_create(name = ERROR)
@@ -205,34 +256,40 @@ def get_lava_job_details(job_id, jenkins_build, lava_server=None):
 
 def create_jenkins_build(jenkins_build, jenkins_db_job, is_umbrella = True, umbrella_db_build = None):
     status_name = RUNNING
-    if not jenkins_build.is_running():
-        status_name = jenkins_build.get_status()
-    build_status, created = JenkinsBuildStatus.objects.get_or_create(name = status_name)
-    jenkins_build_defaults = {
-        "status": build_status,
-        "is_umbrella": is_umbrella,
-        "timestamp": jenkins_build.get_timestamp()
-    }
-    db_build, db_build_created = JenkinsBuild.objects.get_or_create(
-        name = jenkins_build.name,
-        job = jenkins_db_job,
-        number = jenkins_build.get_number(),
-        defaults = jenkins_build_defaults
-        )
-    if not is_umbrella and umbrella_db_build:
-        db_build.umbrella_build = umbrella_db_build
-    if not db_build_created:
-        db_build.status = build_status
-        db_build.is_umbrella = is_umbrella
-        db_build.timestamp = jenkins_build.get_timestamp()
-    db_build.save()
-    if db_build_created:
-        #log.debug("Jenkins build {0} created ({1})".format(jenkins_build.get_number(), jenkins_build.name.decode('ascii', 'ignore')))
-        log.debug("Jenkins build {0} created ({1})".format(jenkins_build.get_number(), ''.join([x for x in jenkins_build.name if ord(x) < 128])))
-    else:
-        #log.debug("Jenkins build {0} updated ({1})".format(jenkins_build.get_number(), jenkins_build.name.decode('ascii', 'ignore')))
-        log.debug("Jenkins build {0} updated ({1})".format(jenkins_build.get_number(), ''.join([x for x in jenkins_build.name if ord(x) < 128])))
-    return db_build
+    try:
+        if not jenkins_build.is_running():
+            status_name = jenkins_build.get_status()
+        build_status, created = JenkinsBuildStatus.objects.get_or_create(name = status_name)
+        jenkins_build_defaults = {
+            "status": build_status,
+            "is_umbrella": is_umbrella,
+            "timestamp": jenkins_build.get_timestamp()
+        }
+        db_build, db_build_created = JenkinsBuild.objects.get_or_create(
+            name = jenkins_build.name,
+            job = jenkins_db_job,
+            number = jenkins_build.get_number(),
+            defaults = jenkins_build_defaults
+            )
+        if not is_umbrella and umbrella_db_build:
+            db_build.umbrella_build = umbrella_db_build
+        if not db_build_created:
+            db_build.status = build_status
+            db_build.is_umbrella = is_umbrella
+            db_build.timestamp = jenkins_build.get_timestamp()
+        db_build.save()
+        if db_build_created:
+            #log.debug("Jenkins build {0} created ({1})".format(jenkins_build.get_number(), jenkins_build.name.decode('ascii', 'ignore')))
+            log.debug("Jenkins build {0} created ({1})".format(jenkins_build.get_number(), ''.join([x for x in jenkins_build.name if ord(x) < 128])))
+        else:
+            #log.debug("Jenkins build {0} updated ({1})".format(jenkins_build.get_number(), jenkins_build.name.decode('ascii', 'ignore')))
+            log.debug("Jenkins build {0} updated ({1})".format(jenkins_build.get_number(), ''.join([x for x in jenkins_build.name if ord(x) < 128])))
+        return db_build
+    except ConnectionError:
+        build_status, created = JenkinsBuildStatus.objects.get_or_create(name = status_name)
+        if umbrella_db_build:
+            umbrella_db_build.status = build_status
+            umbrella_db_build.save()
 
 
 def get_lava_hardware():
